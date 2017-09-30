2153adc83c31c333e65a80d9f1565b88a6dd3528
Rob <git@thepatman.co.uk>
2017-09-30 12:37:25 +0100

    Add files via upload

---
+++ sample-pbconf.py
@@ -0,0 +1,43 @@
+#photobooth configs.
+
+image_path           ='/home/pi/photobooth/images/'
+archive_path        ='/home/pi/photobooth/archive/'
+media_path          ='/home/pi/photobooth/media/'
+background          ='background.jpg'
+overlay             ='overlay.png'
+caption             ='A great photo taken at  '
+fb_upload           =True
+tw_upload           =True
+em_upload           =False
+
+
+
+
+#Facebook stuff
+
+fb_page_id      ='FB PAGE ID'
+fb_access_token ='FB ACCESS TOKEN'
+fb_message      ='Have a look at this photo taken by Pi3 at '
+
+
+
+#Twitter
+
+t_consumer_key        ='TWITTER CONSUMER KEY'
+t_consumer_secret     ='TWITTER CONSUMER SECRET'
+t_access_token        ='TWITTER ACCESSS TOKEN'
+t_access_token_secret ='TWITTER ACCESS TOKEN SECRET'
+
+#mailer
+
+m_fromaddr            ='FROM@EMAIL.ADDRESS'
+m_toaddr              ='TO@EMAIL.ADDRESS'
+m_server            ='MAIL SERVER
+m_user              ='MAIL USER'
+m_pass              ='MAIL PASSWORD'
+
+
+#instagram
+
+i_client_id           ='INSTAGRAM ID'
+i_client_secret       ='INSTAGRAM SECRET'
